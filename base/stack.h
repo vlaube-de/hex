048695848ddaf82ccb7754b62506fce9f50ca867
Yanzheng Li <yanzheng819@gmail.com>
2013-07-04 15:37:49 -0400

    Added base/stack.h and base/stack.c, and added builded target base/stack.o

---
+++ base/stack.h
@@ -0,0 +1,51 @@
+/*
+ * HEX Programming Language
+ * Copyright (C) 2013  Yanzheng Li
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Stack - FILO container */
+
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef struct HexStack_s *Stack;
+
+
+Stack stack_create();
+
+size_t stack_size(Stack stack);
+
+void* stack_top(Stack stack);
+
+int stack_push(Stack stack, void *val, size_t size);
+
+void* stack_pop(Stack stack);
+
+void stack_free(Stack *stack);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _STACK_H_ */
