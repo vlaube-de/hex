b14a0205bd8424b3f1113236bc91fb298b710403
Yanzheng Li <yanzheng819@gmail.com>
2013-08-30 00:01:03 -0700

    Added tests for base/assert.h

--- base/assert.h
+++ base/assert.h
@@ -28,7 +28,7 @@
 
 #define HEX_ASSERT(expr)                                                        \
   do {                                                                          \
-    if(expr == 0) {                                                             \
+    if((expr) == 0) {                                                           \
       fprintf(stderr, "Assertion %s failed, "                                   \
         "at %s[line %d]\n", #expr, __FILE__, __LINE__);                         \
       abort();                                                                  \
