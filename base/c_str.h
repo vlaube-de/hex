ab65a663035bb8f077c169de134da3c7197ba80e
Yanzheng Li <yanzheng819@gmail.com>
2013-08-31 12:22:21 -0700

    Added base/c_str.h

---
+++ base/c_str.h
@@ -0,0 +1,25 @@
+/*
+ * HEX Programming Language
+ * Copyright (C) 2013  Yanzheng Li
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* typedef for char* */
+
+#ifndef _C_STR_H_
+#define _C_STR_H_
+
+typedef char* c_str;
+
+#endif /* _C_STR_H_ */
